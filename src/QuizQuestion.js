2bd227f655fd4e90d9a66396a4f2322362c4415f
alaaali928821 <54397483+alaaali928821@users.noreply.github.com>
2019-08-22 16:53:02 +0200

    delet uncessery constractor

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,9 +1,5 @@
 import React,{Component} from 'react'
 class QuizQuestion extends Component{
-    constructor(props){
-        super(props);
-    }
-
     render(){
         return(     
              <main>
